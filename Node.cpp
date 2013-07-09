0cca180d4ce80c838f1aea04302129ab5172aff8
dekameron22 <artemtkacuk@MacBook-Pro-Artem-2.local>
2013-07-09 21:54:23 +0300

    Node
    Added class Node

---
+++ Node.cpp
@@ -0,0 +1,9 @@
+//
+//  Node.cpp
+//  Hafman
+//
+//  Created by Артем Ткачук on 09.07.13.
+//  Copyright (c) 2013 Артем Ткачук. All rights reserved.
+//
+
+#include "Node.h"
--- Node.h
+++ Node.h
@@ -1,23 +1,14 @@
 //
-//  Tree.h
+//  Node.h
 //  Hafman
 //
 //  Created by Артем Ткачук on 09.07.13.
 //  Copyright (c) 2013 Артем Ткачук. All rights reserved.
 //
 
-#ifndef __Hafman__Tree__
-#define __Hafman__Tree__
+#ifndef __Hafman__Node__
+#define __Hafman__Node__
 
 #include <iostream>
 
-class Tree
-{
-private:
-    
-    
-public:
-    
-};
-
-#endif /* defined(__Hafman__Tree__) */
+#endif /* defined(__Hafman__Node__) */
